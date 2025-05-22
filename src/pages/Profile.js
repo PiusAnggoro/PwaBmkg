d7df226a4b59a1812bb59d8cdb0f52c3b3c66622
Pius Anggoro <piusanggoro@gmail.com>
2025-05-22 14:38:53 +0700

    2nd commit

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,9 @@
 const Profile = () => {
     return (
         <div className="p-4 text-center">
-            <img className="w-[20svh] aspect-square mx-auto my-5 rounded-full" alt="Profile" src="https://c.tenor.com/kglDzTqoEUAAAAAC/tenor.gif" />
-            <h1>Galang Satrio Wicaksono</h1>
-            <h1 className="mb-4">21120122130051</h1>
+            <img className="w-[20svh] aspect-square mx-auto my-5 rounded-full" alt="Profile" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgithub.com%2FPiusAnggoro&psig=AOvVaw3hED1UC_vR1EsOuaLhSSpA&ust=1747985865324000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOj_2vfIto0DFQAAAAAdAAAAABAE" />
+            <h1>Pius Anggoro</h1>
+            <h1 className="mb-4">belajar PWA</h1>
             <a href="https://data.bmkg.go.id" className='text-slate-400'>Data diambil dari data.bmkg.go.id</a>
             <img className="w-[8svh] mx-auto mt-5 rounded-full" alt="BMKG Logo" src="https://pbs.twimg.com/profile_images/2198270324/logo_BMKG_400x400.png" />
             <h1 className="text-2xl">BMKG</h1>
--- src/pages/SplashScreen.js
+++ src/pages/SplashScreen.js
@@ -1,10 +1,9 @@
-// SplashScreen.js
 import React from 'react';
 
 const SplashScreen = () => {
   return (
     <div className="h-screen text-black transition-opacity duration-100 ease-in-out bg-gray-300">
-      <h1 className="relative mx-auto text-4xl font-bold text-center top-1/3">SIGECA</h1>
+      <h1 className="relative mx-auto text-4xl font-bold text-center top-1/3">PWA BMKG</h1>
       <img className='relative w-20 mx-auto mt-8 top-1/2' src='https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif' alt=''/>
     </div>
   );
